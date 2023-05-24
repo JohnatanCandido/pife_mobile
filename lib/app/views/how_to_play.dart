8415eecf18df1dbebe1b448ee21f0870300359ec
Johnatan Cândido <john.acdc@hotmail.com>
2023-05-24 17:39:13 -0300

    Alterações na tela de como jogar.

--- lib/app/views/how_to_play.dart
+++ lib/app/views/how_to_play.dart
@@ -14,11 +14,21 @@ class HowToPlay extends StatelessWidget {
       appBar: const GameAppBar(),
       body: Container(
         padding: const EdgeInsets.all(20),
-        child: Column(
+        child: ListView(
           children: [
+            Text(AppLocalizations.of(context)!.howToPlayTitle1, style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+            const SizedBox(height: 25,),
             Text(AppLocalizations.of(context)!.howToPlay1, textAlign: TextAlign.justify, style: const TextStyle(fontSize: 18),),
             const SizedBox(height: 50,),
+            Text(AppLocalizations.of(context)!.howToPlayTitle2, style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 25,),
             Text(AppLocalizations.of(context)!.howToPlay2, textAlign: TextAlign.justify, style: const TextStyle(fontSize: 18),),
+            const SizedBox(height: 50,),
+            Text(AppLocalizations.of(context)!.howToPlayTitle3, style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 25,),
+            Text(AppLocalizations.of(context)!.howToPlay3, textAlign: TextAlign.justify, style: const TextStyle(fontSize: 18),),
+            const SizedBox(height: 50,),
+            Text(AppLocalizations.of(context)!.howToPlayEnd, style: const TextStyle(fontSize: 18),),
           ],
         ),
       ),
